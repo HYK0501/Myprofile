54759d26bac3fa5e52e69b29a5edee917468fcfb
HYK0501 <codpropm@gmail.com>
2023-09-13 18:36:42 +0000

    Deploying to main from @ HYK0501/Myprofile@3ce961ca1dbf39f2deff5a7e2ddbeb7cfb75b081 🚀

--- js/about.a55fb1cc.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkhykprofile"]=self["webpackChunkhykprofile"]||[]).push([[443],{653:function(n,t,u){u.r(t),u.d(t,{default:function(){return a}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1001),r={},i=(0,l.Z)(r,e,s,!1,null,null,null),a=i.exports}}]);
-//# sourceMappingURL=about.a55fb1cc.js.map
